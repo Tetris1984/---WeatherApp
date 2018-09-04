dc98bcf9b0f21c95fc138a995ebacac0fd900553
Tetris1984 <thegreattetris@gmail.com>
2018-09-04 11:22:35 +0300

    Fixing after talking

--- app/app.js
+++ app/app.js
@@ -3,29 +3,20 @@
 var app = angular.module('weatherApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('main.home');
 
   $stateProvider
     .state('main', {
-      url: '/',
+      url: '',
       templateUrl: 'templates/main.html'
     })
     .state('main.about', {
-      url: '/main/about',
+      url: 'main/about',
       templateUrl: 'templates/about.html'
     })
     .state('main.home', {
-      url: '/main/home',
+      url: 'main/home',
       templateUrl: 'templates/home.html'
 
     })
-    // .state('main.weather', {
-    //   url: '/weather/:name',
-    //   templateUrl: 'templates/city.html',
-    //   controller: 'WeatherCtrl'
-    // });
 })
-
-app.controller('WeatherCtrl', function() {
-  var vm = this;
-});
