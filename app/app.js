5d458617a30214da892508f8bbdaf11d74ab686d
Tetris1984 <thegreattetris@gmail.com>
2018-09-04 16:57:59 +0300

    Remove extra ng-controller

--- app/app.js
+++ app/app.js
@@ -6,28 +6,27 @@ app.config(function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('main/home');
 
   $stateProvider
-    .state('main', {
-      url: '',
-      templateUrl: 'templates/main.html'
-    })
-    .state('main.about', {
-      url: 'main/about',
-      templateUrl: 'templates/about.html'
-    })
-    .state('main.home', {
-      url: 'main/home',
-      templateUrl: 'templates/home.html'
-    })
-    .state('main.weather', {
-        url: "/weather?city",
-        views: {
-          '': {
-            templateUrl: 'templates/city.html',
-            controller: 'WeatherCtrl'
-            }
-          }
-
-    });
+  .state('main', {
+    url: '',
+    templateUrl: 'templates/main.html'
+  })
+  .state('main.about', {
+    url: 'main/about',
+    templateUrl: 'templates/about.html'
+  })
+  .state('main.home', {
+    url: 'main/home',
+    templateUrl: 'templates/home.html'
+  })
+  .state('main.weather', {
+    url: "/weather?city",
+    views: {
+      '': {
+        templateUrl: 'templates/city.html',
+        controller: 'WeatherCtrl'
+      }
+    }
+  });
 });
 
 
