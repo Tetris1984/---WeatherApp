17e690fd75abeef37214dceec48a12cf0b5521ed
Tetris1984 <thegreattetris@gmail.com>
2018-09-04 16:48:30 +0300

    Add city as param to weather route

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@
 var app = angular.module('weatherApp', ['ui.router']);
 
 app.config(function($stateProvider, $urlRouterProvider) {
-  $urlRouterProvider.otherwise('main.home');
+  $urlRouterProvider.otherwise('main/home');
 
   $stateProvider
     .state('main', {
@@ -17,6 +17,20 @@ app.config(function($stateProvider, $urlRouterProvider) {
     .state('main.home', {
       url: 'main/home',
       templateUrl: 'templates/home.html'
-
     })
+    .state('main.weather', {
+        url: "/weather?city",
+        views: {
+          '': {
+            templateUrl: 'templates/city.html',
+            controller: 'WeatherCtrl'
+            }
+          }
+
+    });
+});
+
+
+app.controller('WeatherCtrl', function($scope, $state, $stateParams) {
+  $scope.city = $stateParams.city;
 })
